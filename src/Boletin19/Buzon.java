bf40a43b5776a14a07d7f31ca5ad7a0f034f68cc
ddizoya <ddizoya@eniac30.danielcastelao.org>
2015-02-19 11:13:05 +0100

    Finalizado.

--- src/Boletin19/Buzon.java
+++ src/Boletin19/Buzon.java
@@ -14,15 +14,13 @@ import javax.swing.JOptionPane;
  * @author ddizoya
  */
 public class Buzon {
-
+    
     public ArrayList<Correo> listaCorreos = new ArrayList<Correo>();
-    
-    //Iterator it = listaCorreos.iterator();
-     
-    
-        public void menu() {
-        int option;
 
+    //Iterator it = listaCorreos.iterator();
+    public void menu() {
+        int option;
+        
         do {
             option = Integer.parseInt(JOptionPane.showInputDialog("\"***** MENU ***** \\n\"\n"
                     + "1 --> Añadir. \n"
@@ -30,8 +28,9 @@ public class Buzon {
                     + "3 --> Mostrar. \n"
                     + "4 --> Primer no leído. \n"
                     + "5 --> Número de correos. \n"
-                    + "6 --> Salir "));
-
+                    + "6 --> Mostrar tamaño de la lista. \n"
+                    + "7 --> Salir."));
+            
             switch (option) {
                 case 1:
                     añadir();
@@ -40,7 +39,7 @@ public class Buzon {
                     eliminar();
                     break;
                 case 3:
-                    mostrar();
+                    System.out.println(mostrar());
                     break;
                 case 4:
                     mostrarPrimerNoLeido();
@@ -49,32 +48,37 @@ public class Buzon {
                     numeroCorreos();
                     break;
                 case 6:
-                    porLeer();
+                    numeroCorreos();
                     break;
+                case 7:
+                    System.exit(0);
                 default:
-                    JOptionPane.showMessageDialog(null, "Saliendo del programa...");
+                    JOptionPane.showMessageDialog(null, "Saliendo del programa.");
             }
         } while (option != 7);
     }
-
-
+    
     public int numeroCorreos() {
-
+        
         System.out.println("Hay " + listaCorreos.size() + "correos.");
         return listaCorreos.size();
     }
-
+    
     public void añadir() {
-        Correo y = new Correo();
-        y.setContenido(JOptionPane.showInputDialog("Introduce el contenido: "));
+        Correo y;
+        boolean aux;
+        String contenido = JOptionPane.showInputDialog("Introduce el contenido: ");
         String resp = JOptionPane.showInputDialog("¿Leído? S/N ");
-        if (resp.equalsIgnoreCase("s"))
-            y.setLeido(true);
-            else
-            y.setLeido(false);
+        if (resp.equalsIgnoreCase("s")) {
+            aux = true;
+        } else {
+            aux = false;
+        }
+        y = new Correo(contenido, aux);
         listaCorreos.add(y);
+        
     }
-
+    
     public boolean porLeer() {
         int noLeido = 0, siLeido = 0;
         for (Correo i : listaCorreos) {
@@ -87,26 +91,26 @@ public class Buzon {
         System.out.println("Quedan " + noLeido + " por leer.");
         return true;
     }
-
+    
     public String mostrarPrimerNoLeido() {
         int contador = 0;
         for (Correo i : listaCorreos) {
-            
             if (i.getLeido() == false) {
-                JOptionPane.showMessageDialog(null,"Mensaje no leído en la posición "+ i.getContenido());
+                JOptionPane.showMessageDialog(null, "Mensaje no leído en la posición: " + contador + " Contenido: " + i.getContenido());
+                contador++;
+                i.setLeido(true);
                 break;
-            } 
-            
-            System.out.println(i.getContenido());
+            }
         }
         return null;
     }
-
+    
     public String mostrar() {
-        int k = Integer.parseInt(JOptionPane.showInputDialog("Introduce la posición del mensaje que quieres leer."));
+        int k = Integer.parseInt(JOptionPane.showInputDialog("Introduce la posición del mensaje que quieres leer."));        
         return listaCorreos.get(k).getContenido();
+        
     }
-
+    
     public void eliminar() {
         int k = Integer.parseInt(JOptionPane.showInputDialog("Introduce la posición del mensaje que quieres eliminar."));
         listaCorreos.remove(k);
